6a9956bcd2cd480409baea30613e321a0a6586b7
Juli <yulechka944992@gmail.com>
2025-06-23 02:47:27 +0900

    formated linters widget

--- src/widget.py
+++ src/widget.py
@@ -1,23 +1,21 @@
-from src.masks import get_mask_card_number, get_mask_account
+from src.masks import get_mask_account, get_mask_card_number
 
 
-def mask_account_card( card_number: str) -> str:
+def mask_account_card(card_number: str) -> str:
     """Функция, которая маскирует информацию о картах и счетах"""
     card_number_split = card_number.split()
-    card_number_digit = ''
-    card_number_alpha = ''
+    card_number_digit = ""
+    card_number_alpha = ""
     for card_number_item in card_number_split:
         if card_number_item.isdigit():
             card_number_digit += card_number_item
         if card_number_item.isalpha():
             card_number_alpha += card_number_item
 
-
-    if card_number_alpha == 'Счет':
-        return f'{card_number_alpha} {get_mask_account(card_number_digit)}'
+    if card_number_alpha == "Счет":
+        return f"{card_number_alpha} {get_mask_account(card_number_digit)}"
     else:
-        return card_number. replace(card_number[-16:], get_mask_card_number(card_number[-16:]))
-
+        return card_number.replace(card_number[-16:], get_mask_card_number(card_number[-16:]))
 
 
 def get_date(date: str) -> str | None:
