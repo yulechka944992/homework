5b3f5ef172cd5720dfd311ff37e4877ebe099057
Juli <yulechka944992@gmail.com>
2025-07-07 16:17:23 +0900

    add new test in tests

---
+++ tests/tests_processing.py
@@ -0,0 +1,8 @@
+from src import processing
+
+def test_filter_by_state():
+    pass
+
+
+def test_sort_by_date():
+    pass
---
+++ tests/tests_widget.py
@@ -0,0 +1,8 @@
+from src import widget
+
+def test_mask_account_card():
+    pass
+
+
+def test_get_date():
+    pass
