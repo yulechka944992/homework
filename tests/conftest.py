facd135998fd3099e75a0f49dc048fc04684b9f9
Juli <yulechka944992@gmail.com>
2025-07-19 20:36:02 +0900

    add conftest and fixture

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,7 +1,7 @@
 import pytest
 
 @pytest.fixture
-def list_dict():
+def list_dict() -> list[dict[str, int | str]]:
     return [{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
             {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
             {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
