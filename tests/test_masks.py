e55d2e8bf86b0bab26b85f6b461872eb9a0a408a
Juli <yulechka944992@gmail.com>
2025-07-19 20:35:20 +0900

    typed test_masks.py

--- tests/test_masks.py
+++ tests/test_masks.py
@@ -1,19 +1,19 @@
 from src.masks import get_mask_card_number, get_mask_account
 
 
-def test_get_mask_card_number():
+def test_get_mask_card_number() -> None:
     assert get_mask_card_number("7000792289606361") == "7000 79** **** 6361"
 
 
-def test_empty_string_card_number():
+def test_empty_string_card_number() -> None:
     """Пустая строка возвращает строку с пробелами и звёздочками."""
     assert get_mask_card_number("") == " ** **** "
 
 
-def test_get_mask_account():
+def test_get_mask_account() -> None:
     assert get_mask_account("73654108430135874305") == "**4305"
 
 
-def test_empty_string_mask_account():
+def test_empty_string_mask_account() -> None:
     """Пустая строка возвращает строку со звёздочками."""
     assert get_mask_account("") == "**"
