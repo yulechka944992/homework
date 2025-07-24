e7990e38e528a3c106cea7ac5e5f06664fac6e7a
Juli <yulechka944992@gmail.com>
2025-07-24 23:40:05 +0900

    modified linters

--- src/generators.py
+++ src/generators.py
@@ -14,15 +14,15 @@ def filter_by_currency(transactions_dict: list[dict], currency: str) -> Iterator
 
 def transaction_descriptions(transactions_dict: list) -> Generator[Any, Any, None]:
     """Генератор принимает список словарей с транзакциями
-     и возвращает описание каждой операции по очереди"""
+    и возвращает описание каждой операции по очереди"""
     for transaction in transactions_dict:
         yield transaction["description"]
 
 
-def card_number_generator(start=1, stop=9999999999999999):
+def card_number_generator(start: int=1, stop: int=9999999999999999) -> Generator:
     """Генератор выдает номера банковских карт в формате XXXX XXXX XXXX XXXX"""
     current = start
     while current <= stop:
         num_str = f"{current:016d}"
-        yield ' '.join([num_str[i:i + 4] for i in range(0, 16, 4)])
+        yield " ".join([num_str[i : i + 4] for i in range(0, 16, 4)])
         current += 1
