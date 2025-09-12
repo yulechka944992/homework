0117ac380eb0ed4fc491da13ad55d6b0ef177c1b
Juli <yulechka944992@gmail.com>
2025-09-12 16:57:17 +0900

    formated linters for tests for decorators

--- tests/test_decorators.py
+++ tests/test_decorators.py
@@ -1,13 +1,14 @@
+import os
+
 import pytest
 
 from src.decorators import log
-import os
-
 from src.widget import mask_account_card
 
 
 def test_my_function_success(capsys):
-    """ Проверка на успешное выполнение функции """
+    """Проверка на успешное выполнение функции"""
+
     @log()
     def my_function(x, y):
         return x / y
@@ -18,10 +19,9 @@ def test_my_function_success(capsys):
     assert "my_function ок. Результат: 2.0" in captured.out
 
 
-
-
 def test_my_function_division_by_zero(capsys):
-    """ Проверка обработки ошибки деления на 0 """
+    """Проверка обработки ошибки деления на 0"""
+
     @log()
     def my_function(x, y):
         return x / y
@@ -32,8 +32,10 @@ def test_my_function_division_by_zero(capsys):
     assert result is None
     assert "my_function error: ZeroDivisionError. Inputs: (4, 0), {}\n" in captured.out
 
+
 def test_my_function_log_txt():
-    """ Проверка на вывод в файл """
+    """Проверка на вывод в файл"""
+
     @log(filename="test_log.txt")
     def my_function_sum(x, y):
         return x + y
@@ -46,18 +48,22 @@ def test_my_function_log_txt():
     assert "Функция my_function_sum ок. Результат: 5" in content
     os.remove("test_log.txt")
 
+
 @log()
 def my_function_key_error():
-    return {'a': 1}['b']
+    return {"a": 1}["b"]
+
 
 def test_my_function_key_error(capsys):
-    """ Проверка обработки исключений """
+    """Проверка обработки исключений"""
     my_function_key_error()
     captured = capsys.readouterr()
     assert "my_function_key_error error: KeyError" in captured.out
 
+
 def test_mask_account_card_with_log_success(capsys):
     """Тест успешного маскирования карты с логированием"""
+
     @log()
     def masked_card(card_number):
         return mask_account_card(card_number)
