866f574eb916bb5b7bf8a31bcf0133f8c6e79611
Yuliya <yulechka944992@gmail.com>
2026-01-27 02:04:10 +0900

    new loggers in utils.py

--- src/utils.py
+++ src/utils.py
@@ -1,5 +1,15 @@
 import json
 import os
+import logging
+
+
+logger = logging.getLogger('utils')
+logger.setLevel(logging.DEBUG)
+file_handler = logging.FileHandler("../logs/utils.log", mode='w', encoding='utf-8')
+file_handler.setLevel(logging.INFO)
+file_formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s: %(message)s')
+file_handler.setFormatter(file_formatter)
+logger.addHandler(file_handler)
 
 
 def load_json(file_path: str) -> list[dict]:
@@ -8,6 +18,7 @@ def load_json(file_path: str) -> list[dict]:
     и возвращает список словарей с данными о финансовых транзакциях
     """
     if not os.path.exists(file_path):
+        logger.error("Нет файла по указанному пути.")
         return []
 
     try:
@@ -15,11 +26,13 @@ def load_json(file_path: str) -> list[dict]:
             data = json.load(json_file)
 
             if not isinstance(data, list):
+                logger.error("Файл не является списком!")
                 return []
-
+            logger.info("Файл найден, возвращается список словарей.")
             return data
 
     except (FileNotFoundError, json.JSONDecodeError):
+        logger.error("Ошибка! Файл не найден или не преобразован.")
         return []
 
 
