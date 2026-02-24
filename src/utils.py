f2eae0bccf3e5881f90ac1891f49c9971a95821c
Yuliya <yulechka944992@gmail.com>
2026-02-24 17:22:19 +0900

    comment loggers

--- src/utils.py
+++ src/utils.py
@@ -1,15 +1,15 @@
 import json
 import os
-import logging
-
-
-logger = logging.getLogger('utils')
-logger.setLevel(logging.DEBUG)
-file_handler = logging.FileHandler("../logs/utils.log", mode='w', encoding='utf-8')
-file_handler.setLevel(logging.INFO)
-file_formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s: %(message)s')
-file_handler.setFormatter(file_formatter)
-logger.addHandler(file_handler)
+# import logging
+#
+#
+# logger = logging.getLogger('utils')
+# logger.setLevel(logging.DEBUG)
+# file_handler = logging.FileHandler("../logs/utils.log", mode='w', encoding='utf-8')
+# file_handler.setLevel(logging.INFO)
+# file_formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s: %(message)s')
+# file_handler.setFormatter(file_formatter)
+# logger.addHandler(file_handler)
 
 
 def load_json(file_path: str) -> list[dict]:
@@ -18,7 +18,7 @@ def load_json(file_path: str) -> list[dict]:
     и возвращает список словарей с данными о финансовых транзакциях
     """
     if not os.path.exists(file_path):
-        logger.error("Нет файла по указанному пути.")
+        # logger.error("Нет файла по указанному пути.")
         return []
 
     try:
@@ -26,14 +26,14 @@ def load_json(file_path: str) -> list[dict]:
             data = json.load(json_file)
 
             if not isinstance(data, list):
-                logger.error("Файл не является списком!")
+                # logger.error("Файл не является списком!")
                 return []
-            logger.info("Файл найден, возвращается список словарей.")
+            # logger.info("Файл найден, возвращается список словарей.")
             return data
 
     except (FileNotFoundError, json.JSONDecodeError):
-        logger.error("Ошибка! Файл не найден или не преобразован.")
+        # logger.error("Ошибка! Файл не найден или не преобразован.")
         return []
 
 
-print(load_json("../data/operations.json"))
+# print(load_json("../data/operations.json"))
